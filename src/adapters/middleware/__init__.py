659c0811dd25b406946dea0839eac3819431880c
Pablo Coronel <me@pablitxn.io>
2026-02-10 19:56:12 -0300

    refactor: remove LoggingMiddleware from API exports

--- src/adapters/middleware/__init__.py
+++ src/adapters/middleware/__init__.py
@@ -1,10 +1,8 @@
 """API middleware components."""
 
 from src.adapters.middleware.error_handler import APIError, error_handler_middleware
-from src.adapters.middleware.logging import LoggingMiddleware
 
 __all__ = [
     "APIError",
-    "LoggingMiddleware",
     "error_handler_middleware",
 ]
